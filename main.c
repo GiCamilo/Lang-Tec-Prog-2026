a1865b75a4c806e3ea755ece563bc5e73353c30d
GiCamilo <giovannacds08@gmail.com>
2026-08-05 22:27:43 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int a,b,c,r1,r2;
+
+	a = 10;
+	b = 20;
+	c = 30;
+	r1 = a+b+c;
+	r2 = r1/3;
+
+	printf("A media aritmetica de %d ,%d,  %d = (%d)", a,b,c,r2);
+
+	
+	return 0;
+}
